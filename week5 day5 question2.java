02cd8b915c4acb882e07b488838b08ad55be2a3b
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:34:48 +0530

    Create week5 day5 question2.java

---
+++ week5 day5 question2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        if(a>b)
+        {
+            System.out.print("true");
+        }
+        else
+        {
+            System.out.print("false");
+        }
+    }
+}
