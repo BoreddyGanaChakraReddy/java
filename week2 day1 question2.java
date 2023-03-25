852bf6908fabd92d62b4cc7090126858dc041d35
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 17:59:40 +0530

    Create week2 day1 question2.java

---
+++ week2 day1 question2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int neha=obj.nextInt();
+        int nisha=obj.nextInt();
+        System.out.println(neha+nisha);
+    }
+}
