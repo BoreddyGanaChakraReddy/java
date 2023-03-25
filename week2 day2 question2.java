5a2d12508561805963c9c1e10e1902a30773c5fc
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:00:32 +0530

    Create week2 day2 question2.java

---
+++ week2 day2 question2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        float b=a;
+        
+        if(b<0)
+        {
+            System.out.println("Invalid Input");
+}
+        else
+        {
+            System.out.println(b);
+}
+    }
+}
