7621329ee935869cda355c34ec36240ba02d6258
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 17:56:48 +0530

    Create week1 day5 question1.java

---
+++ week1 day5 question1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int num1,num2,total;
+        Scanner input=new Scanner(System.in);
+        num1=input.nextInt();
+        num2=input.nextInt();
+        total=num1*4+num2*6;
+        System.out.println(total);
+        
+        
+    }
+}
