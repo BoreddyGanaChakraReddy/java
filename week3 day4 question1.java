64839567a55ff70ee2a3e2c5b9cef612ee135124
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:12:23 +0530

    Create week3 day4 question1.java

---
+++ week3 day4 question1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+    Scanner obj=new Scanner(System.in);
+    int n=obj.nextInt();
+        int j;
+    for (int i =n; i >= 1; i--) {
+                for (j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+        }
+       
+    }
