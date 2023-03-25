ee2b59627fcfc67f6c8708b53fb222a9b8431c3f
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:05:58 +0530

    Create week2 day5 question1.java

---
+++ week2 day5 question1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+ {
+Scanner obj=new Scanner(System.in);
+float a=obj.nextFloat();
+int b=obj.nextInt();
+int c=obj.nextInt();
+System.out.print((int)Math.ceil(a));
+System.out.print(" ");
+System.out.print((int)b);
+System.out.print(" ");
+System.out.print((int)c);
+}
+}
