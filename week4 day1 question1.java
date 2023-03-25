058331e69fffb01e16fe3cac946e443af7324866
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:15:38 +0530

    Create week4 day1 question1.java

---
+++ week4 day1 question1.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+    Scanner obj=new Scanner(System.in);
+    String s=obj.nextLine();
+    switch(s)
+    {
+        case "Android":
+{
+System.out.println("Md. Adil Khan"+" "+"3");
+}
+break;         
+        case "Java":
+{
+System.out.println("Shruti"+" "+"4");
+}
+break;     
+        case "OS":
+{
+System.out.println("Puneeth Kumar"+" "+"3");
+}
+break; 
+        case "AI":
+{
+System.out.println("James William"+" "+"2");
+}
+break; 
+
+        default :
+        {
+            System.out.println("This subject is not running in this semester.");
+        }
+        break;
+    }
+    }}
