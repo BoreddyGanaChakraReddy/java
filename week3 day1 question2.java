550402c091fc3deef4adc4cdc097a0892061a00c
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:08:30 +0530

    Create week3 day1 question2.java

---
+++ week3 day1 question2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+    Scanner obj=new Scanner(System.in);
+    int a=obj.nextInt();
+    int i,b;
+    i=a/10;
+    b=i+a;
+    System.out.println(b);
+    int e=(b*100)/12;
+    System.out.println(e/100F);
+   
+    }
+}
