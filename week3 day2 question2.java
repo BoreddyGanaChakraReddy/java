3b54f839cc9dd7ff065a655b837c316256cde34c
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:10:01 +0530

    Create week3 day2 question2.java

---
+++ week3 day2 question2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+     Scanner obj=new Scanner(System.in);
+    int a=obj.nextInt();
+    if(a<0)
+    {
+    System.out.println("Invalid Input");
+    }
+    else
+    {
+    for(int i=1;i<a;i++)
+    {
+        for(int j=1;j<a;j++)
+        {
+              System.out.println("&");
+                          System.out.println("&&");
+
+  
+        }
+    }
+    }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
