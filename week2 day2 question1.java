a0bfba9787429aca29be30dd914240d4bea6339b
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-03-25 18:01:23 +0530

    Create week2 day2 question1.java

---
+++ week2 day2 question1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+    Scanner obj=new Scanner(System.in);
+  float r;
+    r=obj.nextFloat();
+        if(r>0&&r<5)
+        {
+    System.out.println("Thanks");
+  
+        }
+        else
+        {
+                System.out.println("Error: 101");
+
+        }
+    }
+}
