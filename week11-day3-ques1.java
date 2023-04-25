15678f70e1148b96366319927839ed9f272f0f87
BoreddyGanaChakraReddy <125260812+BoreddyGanaChakraReddy@users.noreply.github.com>
2023-04-25 12:15:20 +0530

    Create week11-day3-ques1.java

---
+++ week11-day3-ques1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        int j = s2.length() - 1;
+        boolean eq = true;
+        
+        if(s1.length()-4>=3){
+            for (int i = 0; i < 3 && i < s1.length(); i++) {
+            
+            if (s1.charAt(i) != s2.charAt(j)) {
+                eq = false;
+                break;
+            }
+            j--;
+        }
+            if(eq==true){
+                System.out.println("GREAT");
+            }
+            else{
+                System.out.println("TRY NEXT TIME");
+            }
+       
+        }
+        else{
+            System.out.println("ERROR");
+        }
+        
+    }
+}
